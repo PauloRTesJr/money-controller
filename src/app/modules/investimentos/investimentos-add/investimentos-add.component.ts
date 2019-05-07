a3bf038b4d48f45084cff2b78fc63ac18d4ac7d6
PauloRTesJr <paulortesjr@gmail.com>
2019-05-07 08:34:26 -0300

    update add component

--- src/app/modules/investimentos/investimentos-add/investimentos-add.component.ts
+++ src/app/modules/investimentos/investimentos-add/investimentos-add.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Investimento, Transactions } from 'src/app/shared/models/investimento.model';
+import { IMyDateModel } from 'mydatepicker';
+import UIkit from 'uikit';
 
 @Component({
     selector: 'app-investimentos-add',
@@ -15,12 +17,23 @@ export class InvestimentosAddComponent implements OnInit {
     constructor () { }
 
     ngOnInit() {
-        this.investimentoForm = <Investimento>{};
+        this.investimentoForm = <Investimento>{ transactions: [] };
         this.transactionAdd = <Transactions>{};
     }
 
+    onDateChanged(event: IMyDateModel) {
+        console.log('onDateChanged(): ', event.date, ' - jsdate: ', new Date(event.jsdate).toLocaleDateString(), ' - formatted: ', event.formatted, ' - epoc timestamp: ', event.epoc);
+        this.transactionAdd.date = new Date(event.epoc);
+        console.log("Setting new date", this.transactionAdd.date)
+    }
+
     addTransaction() {
-        this.investimentoForm.transactions.push(this.transactionAdd);
+        if (this.transactionAdd.date && this.transactionAdd.quantity && this.transactionAdd.quantity) {
+            this.investimentoForm.transactions.push(this.transactionAdd);
+            UIkit.modal("#transactions-modal").hide();
+        } else {
+            UIkit.notification('Tempo campo faltando, mano! Preenche tudo aí, carai!', 'danger');
+        }
     }
 
     removeTransaction(index: number) {
