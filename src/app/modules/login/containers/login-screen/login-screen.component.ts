192f6421e90452bb649e5086b64d000f1c3985af
PauloRTesJr <paulortesjr@gmail.com>
2019-05-26 08:09:36 -0300

    start login

---
+++ src/app/modules/login/containers/login-screen/login-screen.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
+
+@Component({
+    selector: 'app-login-screen',
+    templateUrl: './login-screen.component.html',
+    styleUrls: ['./login-screen.component.scss']
+})
+export class LoginScreenComponent implements OnInit {
+
+    constructor (private afAuth: AngularFireAuth) { }
+
+    ngOnInit() {
+    }
+
+}
---
+++ src/app/modules/login/login.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginScreenComponent } from './containers/login-screen/login-screen.component';
+
+@NgModule({
+  declarations: [LoginScreenComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class LoginModule { }
