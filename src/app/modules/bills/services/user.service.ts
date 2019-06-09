ec218d2886783bdeef5e611b4b12d7fea7d48f52
Paulo Tessarolli Jr <paulortesjr@gmail.com>
2019-06-09 18:29:44 -0300

    Update user.service.ts

--- src/app/modules/bills/services/user.service.ts
+++ src/app/modules/bills/services/user.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
+import { Observable } from 'rxjs';
+import { User } from 'src/app/shared/models/user.model';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class UserService {
+    private usersCollection: AngularFirestoreCollection<User>;
+    private users: Observable<User[]>;
 
-  constructor() { }
+    constructor(private db: AngularFirestore) {
+        this.usersCollection = this.db.collection('/users');
+        this.users = this.usersCollection.valueChanges();
+    }
 }
